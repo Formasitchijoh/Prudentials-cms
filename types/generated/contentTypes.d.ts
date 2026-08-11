2bfc051453a44483065f6afb55b5e74b1132c11a
Formasit-chijoh <formasitf@gmail.com>
2026-08-11 03:17:05 +0100

    Add new isSuperAdmin field in users table

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1460,6 +1460,7 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 6;
       }>;
+    isSuperAdmin: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
